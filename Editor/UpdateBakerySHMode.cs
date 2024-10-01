407296ac3f0127ca6127c6d2969a7723944009a0
Silent <16026653+s-ilent@users.noreply.github.com>
2024-10-01 14:13:25 +0930

    Update "UpdateBakerySHMode" to have options for vertex lightmaps.

--- Editor/UpdateBakerySHMode.cs
+++ Editor/UpdateBakerySHMode.cs
@@ -1,34 +1,5 @@
-/*
-This file is free and unencumbered software released into the public domain.
-
-Anyone is free to copy, modify, publish, use, compile, sell, or
-distribute this software, either in source code form or as a compiled
-binary, for any purpose, commercial or non-commercial, and by any
-means.
-
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-
-For more information, please refer to 
-https://www.gamedevblog.com/2012/12/unity-editor-script-for-replacing-shaders.html
-*/
 using UnityEngine;
-
 using UnityEditor;
-
 using System.Collections;
 
 // UpdateBakerySHMode 
@@ -50,13 +21,21 @@ public class UpdateBakerySHMode : ScriptableWizard
         MonoSH = 6
     };
 
-	//public BakeryRenderDirMode oldShader;
+	private enum BakeryVertexLMMode
+	{
+		NoChange = 0,
+		Enable = 1,
+		Disable = 2
+	};
+
 	[SerializeField]
 	private BakeryRenderDirMode newDirectionalMode;
+	[SerializeField]
+	private BakeryVertexLMMode newVertexMode;
 	
 	[MenuItem("Tools/Silent/Update Material Directional Mode for Bakery")]
 
-	static void CreateWizard ()
+	static void CreateWizard()
 	{
 		ScriptableWizard.DisplayWizard<UpdateBakerySHMode>  ("Update Material Directional Mode for Bakery", "Update");
 	}
@@ -69,18 +48,21 @@ public class UpdateBakerySHMode : ScriptableWizard
 		m.SetFloat("_Bakery", 0);
 	}
 
-	void OnWizardCreate ()
+	void OnWizardCreate()
 	{
 		int totalMaterials = 0;
 		int validMaterials = 0;
 		int updatedMaterials = 0;
 
 		Renderer[] renderers = GameObject.FindSceneObjectsOfType (typeof(Renderer)) as Renderer[];
-		foreach (var renderer in renderers) {
-			foreach (Material m in renderer.sharedMaterials) {
+		foreach (var renderer in renderers) 
+		{
+			foreach (Material m in renderer.sharedMaterials) 
+			{
 				totalMaterials++;
 				// Probably only works with Filamented or its templates. Sorry!
-				if (m != null && m.HasProperty("_Bakery")) {
+				if (m != null && m.HasProperty("_Bakery")) 
+				{
                 	float originalBakeryValue = m.GetFloat("_Bakery");
 
 					switch(newDirectionalMode)
@@ -116,6 +98,22 @@ public class UpdateBakerySHMode : ScriptableWizard
 						break;
 
 					}
+
+
+                    switch (newVertexMode)
+                    {
+                        case BakeryVertexLMMode.NoChange:
+                            break;
+                        case BakeryVertexLMMode.Enable:
+                            m.EnableKeyword("_BAKERY_VERTEXLM");
+                            m.SetFloat("_BakeryVertexLM", 1);
+                            break;
+                        case BakeryVertexLMMode.Disable:
+                            m.DisableKeyword("_BAKERY_VERTEXLM");
+                            m.SetFloat("_BakeryVertexLM", 0);
+                            break;
+                    }
+					
 					validMaterials++;
 					if (m.GetFloat("_Bakery") != originalBakeryValue)
 					{
